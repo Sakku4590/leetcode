0cfb721816efc998d5b2087b038c9aa92d7950b2
Sk Sakline Mustaque <123329555+Sakku4590@users.noreply.github.com>
2026-08-06 23:23:07 +0530

    Time: 19 ms (16.4%), Space: 14.9 MB (7.12%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = Counter(nums)
+        for k,v in count.items():
+            if v < 2:
+                return k
+        
