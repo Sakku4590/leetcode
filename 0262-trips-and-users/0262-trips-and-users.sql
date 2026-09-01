1c37ab4ee6d2b0e5a67ce63b450100c589a0a792
Sk Sakline Mustaque <123329555+Sakku4590@users.noreply.github.com>
2026-09-01 23:22:13 +0530

    Time: 943 ms (5.93%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,17 +1,31 @@
-# Write your MySQL query statement below
-select t.request_at as 'Day',
-Round(sum(
-    case when t.status != 'completed' then 1
-    else 0
-    END)/count(*),
-    2
-) as 'Cancellation Rate'
-From Trips as t
-Join Users as c
-On t.client_id = c.users_id
+-- # Write your MySQL query statement below
+-- select t.request_at as 'Day',
+-- Round(sum(
+--     case when t.status != 'completed' then 1
+--     else 0
+--     END)/count(*),
+--     2
+-- ) as 'Cancellation Rate'
+-- From Trips as t
+-- Join Users as c
+-- On t.client_id = c.users_id
+-- join Users as d
+-- on t.driver_id = d.users_id
+-- where c.banned = 'No'
+-- and d.banned = 'No'
+-- and t.request_at between '2013-10-01' and '2013-10-03'
+-- group by t.request_at
+
+select t.request_at as Day,
+    round(sum(
+        case when t.status != 'completed' then 1
+        else 0
+        end)/count(*),2) as 'Cancellation Rate'
+from Trips as t
+join Users as c
+on t.client_id = c.users_id
 join Users as d
 on t.driver_id = d.users_id
-where c.banned = 'No'
-and d.banned = 'No'
-and t.request_at between '2013-10-01' and '2013-10-03'
+where c.banned = 'No' and d.banned = 'No'
+    and t.request_at between '2013-10-01' and '2013-10-03'
 group by t.request_at
